d176b1769acfe135fc25c1f9e6e325546714620b
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-01-25 14:50:34 +0000

    adds numbers and strings code

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,12 +5,27 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5 #output 5
+puts 2 #output 2
 
 # Perform simple math with numbers
+puts 5 + 2 #add
+puts 5 - 2 #subtract
+puts 5 * 2 #multiply
+puts 5.0 / 2.0 #divide, but put decimals so you can get a float (rather than integer)
+puts 5 ** 2 #exponent
+puts (2 + 5) * 5 #order of operations
 
 # Strings
+puts "Hello, world" #needs to be in quotes
 
 # Combine strings together
+puts "Tacos are " + "awesome" #concatenate with plus sign
+
+# Try some weird things
+puts "tacos" * 3 #will print out tacostacostacos
+puts "tacos" + 3 #will print an error because doesn't know how to combine string and integer
+puts "tacos" + 3.to_s #will print tacos3, because this converts 3 to a string (type casting)
 
 # Variables
 
