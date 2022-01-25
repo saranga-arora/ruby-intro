2a380de074abb6dcfe84ad85c88b4572a692ba8d
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-01-25 14:58:46 +0000

    adds variables and strings

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,30 +5,42 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
-puts 5 #output 5
-puts 2 #output 2
+puts 5
+puts 2
 
 # Perform simple math with numbers
-puts 5 + 2 #add
-puts 5 - 2 #subtract
-puts 5 * 2 #multiply
-puts 5.0 / 2.0 #divide, but put decimals so you can get a float (rather than integer)
+puts 5 + 2
+puts 5 - 2
+puts 5 * 2
+puts 5.0 / 2.0 #put decimals so you can get a float (rather than integer)
 puts 5 ** 2 #exponent
-puts (2 + 5) * 5 #order of operations
+puts (2 + 5) * 5
 
 # Strings
-puts "Hello, world" #needs to be in quotes
+puts "Hello, world"
 
 # Combine strings together
-puts "Tacos are " + "awesome" #concatenate with plus sign
+puts "Tacos are " + "awesome"
 
 # Try some weird things
-puts "tacos" * 3 #will print out tacostacostacos
-puts "tacos" + 3 #will print an error because doesn't know how to combine string and integer
-puts "tacos" + 3.to_s #will print tacos3, because this converts 3 to a string (type casting)
+puts "tacos" * 3 #prints out tacostacostacos
+puts "tacos" + 3.to_s #prints tacos3, because this converts 3 to a string (type casting); without it, will get an error
 
 # Variables
+x = 10
+y = 3
+puts x * y
+
+food = "tacos"
+quantity = 3
+puts food * quantity #prints out tacostacostacos
 
 # Combine strings and variables
+first_name = "Boba"
+greeting = "Hello, " + first_name 
+greeting1 = "Hello, #{first_name}" #string interpolation
+
+puts greeting
+puts greeting1 
 
 # String manipulation
