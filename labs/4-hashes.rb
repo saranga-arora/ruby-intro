00dbdae77e2ec34b5d053931d740ceff49927eb0
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-01-25 17:02:29 +0000

    adds code

--- labs/4-hashes.rb
+++ labs/4-hashes.rb
@@ -7,14 +7,7 @@
 # write a weather summary out to the screen. Something like this:
 
 # Currently it is 67 degrees and sunny. Tomorrow it will be 65 and Mostly Cloudy.
-# The next day it we will see Partly Cloudy skies, with a temperature of 70.
-
-# HINT
-# String interpolation is a fancy term for "we don't have
-# to add strings together using the plus sign anymore"
-# number_of_tacos = 5
-# "I would like #{number_of_tacos} tacos, please"
-# No more worrying about converting numbers to strings!
+# The next day we will see Partly Cloudy skies, with a temperature of 70.
 
 weather_data = {
   current: {
@@ -26,3 +19,7 @@ weather_data = {
     { temperature: 70, conditions: "Partly Cloudy" }
   ]
 }
+
+puts "Currently it is #{weather_data[:current][:temperature]} degrees and sunny."
+puts "Tomorrow it will be #{weather_data[:forecast][0][:temperature]} and #{weather_data[:forecast][0][:conditions]}."
+puts "The next day we will see #{weather_data[:forecast][1][:conditions]} skies, with a temperature of #{weather_data[:forecast][1][:temperature]}."
