229706806ec464ce3890bf8a6811f75c0570b043
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-01-25 17:27:27 +0000

    adds code

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -2,8 +2,17 @@
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
 
-things_that_would_be_better_with_bacon = ["tacos", "cheeseburgers", "donuts"]
+things_that_would_be_better_with_bacon = ["tacos", "cheeseburgers", "donuts", "unicorns"]
 
 # The Old Way
+puts "bacon #{things_that_would_be_better_with_bacon[0]}"
+puts "bacon #{things_that_would_be_better_with_bacon[1]}"
+puts "bacon #{things_that_would_be_better_with_bacon[2]}"
+puts "bacon #{things_that_would_be_better_with_bacon[3]}"
 
 # The New Way
+for food in things_that_would_be_better_with_bacon
+    #1st time...food = tacos
+    #2nd time...food = cheeseburgers
+    puts "bacon #{food}"
+end 
