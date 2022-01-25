8a36f8ae4d2380e0ed71adfc07e703dbbc977aaa
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-01-25 16:56:12 +0000

    adds code

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,26 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+    name: "Saranga",
+    location: {                                             #this is a hash within the hash
+        city: "Evanston",
+        state: "IL"
+    },
+    status: "Working in class!",
+    timeline: [                                             #this is a hash within an array within the hash
+        {status: "Teaching this class", posted: "9am" }, 
+        {status: "Eating tacos", posted: "7am"}
+    ]
+}
+
+puts profile #will print out entire hash, in code format
+
+#Outputting data from the hash
+puts profile[:name] 
+puts profile[:status]
 
 # Accessing data from the hash
-
-# More Complex Hashes
+puts profile[:location] #will print out the location hash, in code format
+puts profile[:location][:city] #will print out evanston
+puts profile[:timeline][1][:status] #will print out "eating tacos"
