7b7b6dab1c0227c13113916e679bfd96cc2f8334
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-01-25 16:20:11 +0000

    adds code

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -3,10 +3,29 @@
 # ruby 3-arrays.rb
 
 # Arrays are lists of things
+favorite_foods = ["tacos", "cookies", "nachos", "ice cream"]
+puts favorite_foods #outputs list
+puts favorite_foods.inspect #outputs array in the code format
+
+number_list = [4, 8, 15, 16, 23, 42]
+puts number_list 
+
+mixed_array = ["tacos", 12, true]
+puts mixed_array 
+
+shopping_list = [["kale", "raisins", "sticks"], ["beer", "tacos"]]
+puts shopping_list 
 
 # Accessing the array
+puts favorite_foods [0]
+puts favorite_foods [1] 
+puts favorite_foods [-1] #this will give us the item that is in last position of array
+
+puts shopping_list [1][1] #use this to get tacos
+
 
 # Add to the array
-
-# There are lots of fun things you can do with arrays:
-# https://ruby-doc.org/core-2.7.0/Array.html
+favorite_foods.push("more tacos") #adds an element to end of array
+favorite_foods = favorite_foods + ["fries", "ramen"] #adds another array to the array
+puts favorite_foods.inspect 
+puts favorite_foods.size 
