40c5081899d4f56312c28151905bc9fd86ccff4a
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-01-28 20:34:37 +0000

    adds code

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -8,3 +8,25 @@
 # screen.
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+for suit in suits 
+    for rank in ranks
+        output = "#{rank} of #{suit}"
+        puts output
+    end
+end
+
+# Not as simple, but assigns the deck to a variable so we can use it for other things
+
+deck = [] # Build an empty array
+
+for rank in ranks
+  for suit in suits
+    card = "#{rank} of #{suit}" # adds each card to the array
+    deck.push(card) #adds element to end of array
+  end
+end
+
+shuffled_deck = deck.shuffle # Shuffle the deck
+puts shuffled_deck[0] # Take one off the top
+puts deck.sample # Or pull out a random card
